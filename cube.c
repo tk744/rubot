c550490aa06d545c2c535080e585eb3d2a460ef8
Tushar Khan <tkbot744@gmail.com>
2021-11-08 17:30:55 -0500

    brand new efficient cube model implementation using bitmasks

--- cube.c
+++ cube.c
@@ -1,419 +1,157 @@
 #include <stdint.h>
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 
-/* This source file implements the state and behavior of a Rubik's cube. */
+#define COLOR_BITS 3
 
-/* TODO:
- * 1. Finish X, Y, Z transformations in move() function.
- * 1. Implement orientation in move() function.
- * 2. Implement is_solved() function. 
- * 3. Fix and test cube state save and load functions.
- * 4. Documentation about how enums relate to state. */
+typedef uint8_t Color, Pos, Move;
+typedef uint32_t Face;
+typedef struct { Face U, D, L, R, F, B; } Cube;
 
-/* Global constants for individual cubie information. 
- * Type uint8_t instead of enum because it reduces cube state memory by 4x. */
+const Color WHITE = 1, YELLOW = 2, RED = 3, \
+            ORANGE = 4, BLUE = 5, GREEN = 6;
 
-// center cubie positions
-uint8_t UP = 0, DOWN = 1, LEFT = 2, RIGHT = 3, FRONT = 4, BACK = 5;
-// edge cubie positions
-uint8_t UL = 0, UR = 1, UF = 2, UB = 3, DL = 4, DR = 5, \
-        DF = 6, DB = 7, LF = 8, LB = 9, RF = 10, RB = 11;
-// corner cubie positions
-uint8_t ULF = 0, ULB = 1, URF = 2, URB = 3, \
-        DLF = 4, DLB = 5, DRF = 6, DRB = 7;
+const Pos   CC = 0, UL = 1, UU = 2, UR = 3, RR = 4, \
+            DR = 5, DD = 6, DL = 7, LL = 8;
 
-// center cubie colors
-uint8_t WHITE = 0, YELLOW = 1, GREEN = 2, \
-        BLUE = 3, RED = 4, ORANGE = 5;
-// edge cubie colors
-uint8_t WG = 0, WB = 1, WR = 2, WO = 3, YG = 4, YB = 5, \
-        YR = 6, YO = 7, GR = 8, GO = 9, BR = 10, BO = 11;
-// corner cubie colors
-uint8_t WRG = 0, WRB = 1, WOG = 2, WOB = 3, \
-        YRG = 4, YRB = 5, YOG = 6, YOB = 7;
+const Move  U=1,  UI=2,  // face rotations
+            D=3,  DI=4,
+            L=5,  LI=6,
+            R=7,  RI=8,
+            F=9,  FI=10,
+            B=11, BI=12,
+            X=13, XI=14, // cube rotations
+            Y=15, YI=16,
+            Z=17, ZI=18,
+            NOP=0;       // no rotation
 
-// edge (x2) and corner (x3) orientations
-uint8_t O1 = 1, O2 = 2, O3 = 3;
+Color readFace(Face f, Pos p) {
+    return (f >> (COLOR_BITS * p)) & 0b111;
+}
 
-/* Cube state definition.
- * Represented by lists of center, edge, and corner cubies.
- * Each list is indexed by valid cubie position.
- * E.g. centers[UP], edges[UL], and corners[ULF] are valid. */
-struct Cube {
-    struct Center {
-        uint8_t color;
-    } centers[6];
+Face writeFace(Face f, Pos p, Color c) {
+    Face clear_mask = ~(0b111 << (COLOR_BITS * p));
+    Face write_mask = c << (COLOR_BITS * p);
+    return f & clear_mask | write_mask;
+}
+
+Face rotateFaceClockwise(Face f) {
+    Color cc = readFace(f, CC);
+    Color dl = readFace(f, DL), ll = readFace(f, LL);
     
-    struct Edge {
-        uint8_t color;
-        uint8_t orientation;
-    } edges[12];
+    f = f << (2*COLOR_BITS) | f >> (32 - (2*COLOR_BITS));
 
-    struct Corner {
-        uint8_t color;
-        uint8_t orientation;
-    } corners[8];
-};
+    f = writeFace(f, CC, cc);
+    f = writeFace(f, UL, dl);
+    f = writeFace(f, UU, ll);
 
-/* Convert a cube state into a string. */
-/*
-char *save_cube(struct Cube *st) {
-    char str[sizeof(struct Cube)/sizeof(uint8_t)+1]; // +1 for terminal character
+    return f;
+}
 
-    short int i = 0;
-    for ( ; i<6+(2*12)+(2*8) ; i++ ) {
-        if (i<6) {                  // center cubie colors
-            sprintf(str+i, "%c", 'a' + st->centers[i].color);
-        }
-        else if (i<6+12) {          // edge cubie colors
-            sprintf(str+i, "%c", 'a' + st.edges[i-6].color);
-        }
-        else if (i<6+(2*12)) {      // edge cubie orientations
-            sprintf(str+i, "%c", 'a' + st.edges[i-(6+12)].orientation);
-        }
-        else if (i<6+(2*12)+8) {    // corner cubie colors
-            sprintf(str+i, "%c", 'a' + st.corners[i-(6+(2*12))].color);
-        }
-        else {                      // corner cubie orientations
-            sprintf(str+i, "%c", 'a' + st.corners[i-(6+(2*12)+8)].orientation);
-        }
+Face rotateFaceCClockwise(Face f) {
+    Color cc = readFace(f, CC);
+    Color ul = readFace(f, UL), uu = readFace(f, UU);
+    
+    f = f >> (2*COLOR_BITS) | f << (32 - (2*COLOR_BITS));
+
+    f = writeFace(f, CC, cc);
+    f = writeFace(f, DL, ul);
+    f = writeFace(f, LL, uu);
+
+    return f;
+}
+
+Cube transform(Cube c, Move mv) {
+    // no rotation
+    if (mv == NOP) {
+        return c;
     }
 
-    return str;
-}
-*/
-
-/* Convert a string into a cube state. */
-/*
-struct Cube load_cube(char *str) {
-    struct Cube st;
-
-    short int i = 0, v;
-    for ( ; i<6+(2*12)+(2*8) ; i++ ) {
-        // TODO: fix second argument of this line
-        v = strtol(str+i, str+(i+1), 0) - 'a';
-        if (i<6) {                  // center cubie colors
-            st.centers[i].color = v;
-        }
-        else if (i<6+12) {          // edge cubie colors
-            st.edges[i-6].color = v;
-        }
-        else if (i<6+(2*12)) {      // edge cubie orientations
-            st.edges[i-(6+12)].orientation = v;
-        }
-        else if (i<6+(2*12)+8) {    // corner cubie colors
-            st.corners[i-(6+(2*12))].color = v;
-        }
-        else {                      // corner cubie orientations
-            st.corners[i-(6+(2*12)+8)].orientation = v;
-        }
+    // face rotations
+    if (mv == U || mv == Y) {
+        c.U = rotateFaceClockwise(c.U);
+    }
+    else if (mv == UI || mv == YI) {
+        c.U = rotateFaceCClockwise(c.U);
+    }
+    else if (mv == D || mv == YI) {
+        c.D = rotateFaceClockwise(c.D);
+    }
+    else if (mv == DI || mv == Y) {
+        c.D = rotateFaceCClockwise(c.D);
+    }
+    else if (mv == L || mv == XI) {
+        c.L = rotateFaceClockwise(c.L);
+    }
+    else if (mv == LI || mv == X) {
+        c.L = rotateFaceCClockwise(c.L);
+    }
+    else if (mv == R || mv == X) {
+        c.R = rotateFaceClockwise(c.R);
+    }
+    else if (mv == RI || mv == XI) {
+        c.R = rotateFaceCClockwise(c.R);
+    }
+    else if (mv == F || mv == Z) {
+        c.F = rotateFaceClockwise(c.F);
+    }
+    else if (mv == FI || mv == ZI) {
+        c.F = rotateFaceCClockwise(c.F);
+    }
+    else if (mv == B || mv == ZI) {
+        c.B = rotateFaceClockwise(c.B);
+    }
+    else if (mv == BI || mv == Z) {
+        c.B = rotateFaceCClockwise(c.B);
     }
 
-    return st;
+    // edge and corner rotations
+    
 }
-*/
 
-/* TODO: Returns 1 if cube `st` is in a solved state, 0 otherwise. */
-int is_solved(struct Cube st) {
+int isSolved(Cube c) {
     return 0;
 }
 
-/* Set of all possible transformations on a cube state. */
-enum Move {
-    // no rotation 
-    NOP = 0,
-
-    // face rotations
-    U, UI, U2, UI2,
-    D, DI, D2, DI2,
-    L, LI, L2, LI2,
-    R, RI, R2, RI2,
-    F, FI, F2, FI2,
-    B, BI, B2, BI2,
-    
-    // cube rotations
-    X, XI,
-    Y, YI,
-    Z, ZI
-};
-
-/* Returns the cube state obtained by applying the move `mv` to cube `st`.
- * Does not modify the original cube `st`. */
-struct Cube move(struct Cube st, enum Move mv) {
-    // half-turn transformations
-    if (mv == U2) {
-        return move(move(st, U), U);
-    }
-    else if (mv == UI2) {
-        return move(move(st, UI), UI);
-    }
-    else if (mv == D2) {
-        return move(move(st, D), D);
-    }
-    else if (mv == DI2) {
-        return move(move(st, DI), DI);
-    }
-    else if (mv == L2) {
-        return move(move(st, L), L);
-    }
-    else if (mv == LI2) {
-        return move(move(st, LI), LI);
-    }
-    else if (mv == R2) {
-        return move(move(st, R), R);
-    }
-    else if (mv == RI2) {
-        return move(move(st, RI), RI);
-    }
-    else if (mv == F2) {
-        return move(move(st, F), F);
-    }
-    else if (mv == FI2) {
-        return move(move(st, FI), FI);
-    }
-    else if (mv == B2) {
-        return move(move(st, B), B);
-    }
-    else if (mv == BI2) {
-        return move(move(st, BI), BI);
-    }
-    
-    // center cubie transformations
-    uint8_t tmp_center;
-    if (mv == X) {
-        tmp_center = st.centers[UP].color;
-        st.centers[UP].color = st.centers[FRONT].color;
-        st.centers[FRONT].color = st.centers[DOWN].color;
-        st.centers[DOWN].color = st.centers[BACK].color;
-        st.centers[BACK].color = tmp_center;
-    }
-    else if (mv == XI) {
-        tmp_center = st.centers[UP].color;
-        st.centers[UP].color = st.centers[BACK].color;
-        st.centers[BACK].color = st.centers[DOWN].color;
-        st.centers[DOWN].color = st.centers[FRONT].color;
-        st.centers[FRONT].color = tmp_center;
-    }
-    else if (mv == Y) {
-
-    }
-    else if (mv == YI) {
-
-    }
-    else if (mv == Z) {
-
-    }
-    else if (mv == ZI) {
-
-    }
-    
-    // edge and corner cubie transformations
-    uint8_t tmp_edge, tmp_corner;
-    if (mv == U || mv == Y) {
-        tmp_edge = st.edges[UF].color;
-        st.edges[UF].color = st.edges[UR].color;
-        st.edges[UR].color = st.edges[UB].color;
-        st.edges[UB].color = st.edges[UL].color;
-        st.edges[UL].color = tmp_edge;
-
-        tmp_corner = st.corners[URF].color;
-        st.corners[URF].color = st.corners[URB].color;
-        st.corners[URB].color = st.corners[ULB].color;
-        st.corners[ULB].color = st.corners[ULF].color;
-        st.corners[ULF].color = tmp_corner;
-    }
-    if (mv == UI || mv == YI) {
-        tmp_edge = st.edges[UF].color;
-        st.edges[UF].color = st.edges[UL].color;
-        st.edges[UL].color = st.edges[UB].color;
-        st.edges[UB].color = st.edges[UR].color;
-        st.edges[UR].color = tmp_edge;
-
-        tmp_corner = st.corners[URF].color;
-        st.corners[URF].color = st.corners[ULF].color;
-        st.corners[ULF].color = st.corners[ULB].color;
-        st.corners[ULB].color = st.corners[URB].color;
-        st.corners[URB].color = tmp_corner;
-    }
-    if (mv == D || mv == YI) {
-        tmp_edge = st.edges[DF].color;
-        st.edges[DF].color = st.edges[DL].color;
-        st.edges[DL].color = st.edges[DB].color;
-        st.edges[DB].color = st.edges[DR].color;
-        st.edges[DR].color = tmp_edge;
-
-        tmp_corner = st.corners[DRF].color;
-        st.corners[DRF].color = st.corners[DLF].color;
-        st.corners[DLF].color = st.corners[DLB].color;
-        st.corners[DLB].color = st.corners[DRB].color;
-        st.corners[DRB].color = tmp_corner;
-    }
-    if (mv == DI || mv == Y) {
-        tmp_edge = st.edges[DF].color;
-        st.edges[DF].color = st.edges[DR].color;
-        st.edges[DR].color = st.edges[DB].color;
-        st.edges[DB].color = st.edges[DL].color;
-        st.edges[DL].color = tmp_edge;
-
-        tmp_corner = st.corners[DRF].color;
-        st.corners[DRF].color = st.corners[DRB].color;
-        st.corners[DRB].color = st.corners[DLB].color;
-        st.corners[DLB].color = st.corners[DLF].color;
-        st.corners[DLF].color = tmp_corner;
-    }
-    if (mv == R || mv == X) {
-        tmp_edge = st.edges[RF].color;
-        st.edges[RF].color = st.edges[DR].color;
-        st.edges[DR].color = st.edges[RB].color;
-        st.edges[RB].color = st.edges[UR].color;
-        st.edges[UR].color = tmp_edge;
-
-        tmp_corner = st.corners[DRF].color;
-        st.corners[DRF].color = st.corners[DRB].color;
-        st.corners[DRB].color = st.corners[URB].color;
-        st.corners[URB].color = st.corners[URF].color;
-        st.corners[URF].color = tmp_corner;
-    }
-    if (mv == RI || mv == XI) {
-        tmp_edge = st.edges[RF].color;
-        st.edges[RF].color = st.edges[UR].color;
-        st.edges[UR].color = st.edges[RB].color;
-        st.edges[RB].color = st.edges[DR].color;
-        st.edges[DR].color = tmp_edge;
-
-        tmp_corner = st.corners[URF].color;
-        st.corners[URF].color = st.corners[URB].color;
-        st.corners[URB].color = st.corners[DRB].color;
-        st.corners[DRB].color = st.corners[DRF].color;
-        st.corners[DRF].color = tmp_corner;
-    }
-    if (mv == L || mv == XI) {
-        tmp_edge = st.edges[LF].color;
-        st.edges[LF].color = st.edges[UL].color;
-        st.edges[UL].color = st.edges[LB].color;
-        st.edges[LB].color = st.edges[DL].color;
-        st.edges[DL].color = tmp_edge;
-
-        tmp_corner = st.corners[ULF].color;
-        st.corners[ULF].color = st.corners[ULB].color;
-        st.corners[ULB].color = st.corners[DLB].color;
-        st.corners[DLB].color = st.corners[DLF].color;
-        st.corners[DLF].color = tmp_corner;
-    }
-    if (mv == LI || mv == X) {
-        tmp_edge = st.edges[LF].color;
-        st.edges[LF].color = st.edges[DL].color;
-        st.edges[DL].color = st.edges[LB].color;
-        st.edges[LB].color = st.edges[UL].color;
-        st.edges[UL].color = tmp_edge;
-
-        tmp_corner = st.corners[DLF].color;
-        st.corners[DLF].color = st.corners[DLB].color;
-        st.corners[DLB].color = st.corners[ULB].color;
-        st.corners[ULB].color = st.corners[ULF].color;
-        st.corners[ULF].color = tmp_corner;
-    }
-    if (mv == F || mv == Z) {
-        tmp_edge = st.edges[UF].color;
-        st.edges[UF].color = st.edges[LF].color;
-        st.edges[LF].color = st.edges[DF].color;
-        st.edges[DF].color = st.edges[RF].color;
-        st.edges[RF].color = tmp_edge;
-
-        tmp_corner = st.corners[URF].color;
-        st.corners[URF].color = st.corners[ULF].color;
-        st.corners[ULF].color = st.corners[DLF].color;
-        st.corners[DLF].color = st.corners[DRF].color;
-        st.corners[DRF].color = tmp_corner;
-    }
-    if (mv == FI || mv == ZI) {
-        tmp_edge = st.edges[UF].color;
-        st.edges[UF].color = st.edges[RF].color;
-        st.edges[RF].color = st.edges[DF].color;
-        st.edges[DF].color = st.edges[LF].color;
-        st.edges[LF].color = tmp_edge;
-
-        tmp_corner = st.corners[ULF].color;
-        st.corners[ULF].color = st.corners[URF].color;
-        st.corners[URF].color = st.corners[DRF].color;
-        st.corners[DRF].color = st.corners[DLF].color;
-        st.corners[DLF].color = tmp_corner;
-    }
-    if (mv == B || mv == ZI) {
-        tmp_edge = st.edges[UB].color;
-        st.edges[UB].color = st.edges[RB].color;
-        st.edges[RB].color = st.edges[DB].color;
-        st.edges[DB].color = st.edges[LB].color;
-        st.edges[LB].color = tmp_edge;
-
-        tmp_corner = st.corners[ULB].color;
-        st.corners[ULB].color = st.corners[URB].color;
-        st.corners[URB].color = st.corners[DRB].color;
-        st.corners[DRB].color = st.corners[DLB].color;
-        st.corners[DLB].color = tmp_corner;
-    }
-    if (mv == BI || mv == Z) {
-        tmp_edge = st.edges[UB].color;
-        st.edges[UB].color = st.edges[LB].color;
-        st.edges[LB].color = st.edges[DB].color;
-        st.edges[DB].color = st.edges[RB].color;
-        st.edges[RB].color = tmp_edge;
-
-        tmp_corner = st.corners[URB].color;
-        st.corners[URB].color = st.corners[ULB].color;
-        st.corners[ULB].color = st.corners[DLB].color;
-        st.corners[DLB].color = st.corners[DRB].color;
-        st.corners[DRB].color = tmp_corner;
-    }
-    
-    return st;
+void printFace(Face f) {
+    printf("%d %d %d\n", readFace(f, UL), readFace(f, UU), readFace(f, UR));
+    printf("%d %d %d\n", readFace(f, LL), readFace(f, CC), readFace(f, RR));
+    printf("%d %d %d\n", readFace(f, DL), readFace(f, DD), readFace(f, DR));
 }
 
-/* Writes the shortest list of moves into `mvs` which orients the cube `st` 
- * with the white face down and red face FRONT.
- * Returns the number of moves in the sequence.
- * Requires that the length of `mvs` is at least 4. 
- */
-int orient(enum Move *mvs, struct Cube st) {
-    int i = 0;
-    while (st.centers[DOWN].color != WHITE || st.centers[FRONT].color != RED) {
-        // TODO: go thru all cases
-        enum Move mv;
-        // 1-move away
-        if (st.centers[BACK].color == WHITE && st.centers[DOWN].color == RED) {
-            mv = X;
-        }
-        else if (st.centers[FRONT].color == WHITE && st.centers[UP].color == RED) {
-            mv = XI;
-        }
-        else if (st.centers[LEFT].color == WHITE && st.centers[FRONT].color == RED) {
-            mv = ZI;
-        }
-        else if (st.centers[RIGHT].color == WHITE && st.centers[FRONT].color == RED) {
-            mv = Z;
-        }
-        else if (st.centers[DOWN].color == WHITE && st.centers[LEFT].color == RED) {
-            mv = YI;
-        }
-        else if (st.centers[DOWN].color == WHITE && st.centers[RIGHT].color == RED) {
-            mv = Y;
-        }
-        else if (st.centers[UP].color == WHITE && st.centers[BACK].color == RED) {
-            mv = X;
-        }
-        // 2-moves away ...
-        // 3-moves away ...
-        else {
-            // TODO: error handling
-            return -1;
-        }
-        st = move(st, (mvs[i++] = mv));
-    }
+void printCube(Cube c) {
+    printf("  U     L     F     R     B     D  ");
 
-    return i;
+    printf("%d %d %d | ", readFace(c.U, UL), readFace(c.U, UU), readFace(c.U, UR));
+    printf("%d %d %d | ", readFace(c.L, UL), readFace(c.L, UU), readFace(c.L, UR));
+    printf("%d %d %d | ", readFace(c.F, UL), readFace(c.F, UU), readFace(c.F, UR));
+    printf("%d %d %d | ", readFace(c.R, UL), readFace(c.R, UU), readFace(c.R, UR));
+    printf("%d %d %d | ", readFace(c.B, UL), readFace(c.B, UU), readFace(c.B, UR));
+    printf("%d %d %d \n", readFace(c.D, UL), readFace(c.D, UU), readFace(c.D, UR));
+
+    printf("%d %d %d | ", readFace(c.U, LL), readFace(c.U, CC), readFace(c.U, RR));
+    printf("%d %d %d | ", readFace(c.L, LL), readFace(c.L, CC), readFace(c.L, RR));
+    printf("%d %d %d | ", readFace(c.F, LL), readFace(c.F, CC), readFace(c.F, RR));
+    printf("%d %d %d | ", readFace(c.R, LL), readFace(c.R, CC), readFace(c.R, RR));
+    printf("%d %d %d | ", readFace(c.B, LL), readFace(c.B, CC), readFace(c.B, RR));
+    printf("%d %d %d \n", readFace(c.D, LL), readFace(c.D, CC), readFace(c.D, RR));
+
+    printf("%d %d %d | ", readFace(c.U, DL), readFace(c.U, DD), readFace(c.U, DR));
+    printf("%d %d %d | ", readFace(c.L, DL), readFace(c.L, DD), readFace(c.L, DR));
+    printf("%d %d %d | ", readFace(c.F, DL), readFace(c.F, DD), readFace(c.F, DR));
+    printf("%d %d %d | ", readFace(c.R, DL), readFace(c.R, DD), readFace(c.R, DR));
+    printf("%d %d %d | ", readFace(c.B, DL), readFace(c.B, DD), readFace(c.B, DR));
+    printf("%d %d %d \n", readFace(c.D, DL), readFace(c.D, DD), readFace(c.D, DR));
 }
 
 int main() {
-    printf("%d bytes required to store cube state.", sizeof(struct Cube));
+    Face u, d, l, r, f, b;
+    writeFace(u, CC, WHITE);
+
+    Face f1 = 51396313;
+    Face f2 = rotateFaceCClockwise(f1);
+    printFace(f1);
+    printf("\n");
+    printFace(f2);
+    printf("%d bytes required to core cube cate.", sizeof(Cube));
 }
