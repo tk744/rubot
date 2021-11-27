f0538bfe1ce1629ad7f35b44c69face1417e722a
Tushar Khan <tkbot744@gmail.com>
2021-11-27 01:54:40 -0500

    update edge orientation and broken print cube

--- include/cube.h
+++ include/cube.h
@@ -18,6 +18,7 @@ typedef unsigned long long Int64;
 
 typedef Int8 Move, MoveMask;
 typedef Int8 CubieEnum;
+typedef Int8 FaceEnum;
 
 typedef struct {
     Int64 edges;
@@ -29,10 +30,12 @@ typedef struct {
 // 1-hot encoding masks
 static const MoveMask NOP=0, U=1, D=2, F=4, B=8, R=16, L=32, I=64, H=128;
 
-// edge cubies
-static const CubieEnum UF=0, UB=1, UR=2, UL=3, FR=4, FL=5, DF=6, DB=7, DR=8, DL=9, BR=10, BL=11;
-// corner cubies
+// edge cubie encoding index
+static const CubieEnum UF=0, UB=1, UR=2, UL=3, DF=4, DB=5, DR=6, DL=7, FR=8, FL=9, BR=10, BL=11;
+// corner cubie encoding index
 static const CubieEnum UFR=0, UFL=1, UBR=2, UBL=3, DFR=4, DFL=5, DBR=6, DBL=7;
+
+static FaceEnum U_FACE=1, D_FACE=2, F_FACE=3, B_FACE=4, R_FACE=5, L_FACE=6;
 
 /* FUNCTION PROTOTYPES */
 
--- src/cube.c
+++ src/cube.c
@@ -3,14 +3,13 @@
 
 /*
 TODO:
-1. applyMove() orientations
-2. printCube()
+1. applyMove() update corner orientation
+2. getColor()
 */
 
 /* MODEL CONSTANTS */
 
-#define EDGE_BITS 5
-#define CORNER_BITS 5
+#define CUBIE_BITS 5
 
 /* FUNCTION IMPLEMENTATION */
 
@@ -19,15 +18,19 @@ Cube cubeFactory() {
 
     Int64 i;
     for(i=0 ; i<NUM_EDGES ; i++) {
-        edges |= i << (EDGE_BITS * i);
+        edges |= i << (CUBIE_BITS * i);
     }
     for(i=0 ; i<NUM_CORNERS ; i++) {
-        corners |= i << (CORNER_BITS * i);
+        corners |= i << (CUBIE_BITS * i);
     }
 
     // 407901468851537952 247132686368
     Cube c = { edges, corners };
     return c;
+}
+
+static Int8 getCubie(Int64 encoding, CubieEnum ce) {
+    return (encoding >> (CUBIE_BITS * ce)) & ((1 << CUBIE_BITS) - 1);
 }
 
 Cube applyMove(Cube c, Move m) {
@@ -88,28 +91,37 @@ Cube applyMove(Cube c, Move m) {
         return c;
     }
 
-    // rotate permutations and compute orientations of affected cubies
+    // update permutations and orientations of affected cubies
     Cube old_c = c;
-    int i, b;
+    int i, isEdge;
     for (i=0 ; i<4 ; i++) {
-        for (b=0 ; b<2 ; b++) {
+        // alternate between edge and corner cubies
+        for (isEdge=0 ; isEdge<2 ; isEdge++) {
+            // get encoding index of old and new cubie
             CubieEnum old_enum, new_enum;
-            old_enum = b ? edge_enums[i] : corner_enums[i];
-            new_enum = b ? edge_enums[(i+1)%4] : corner_enums[(i+1)%4];
+            old_enum = isEdge ? edge_enums[i] : corner_enums[i];
+            new_enum = isEdge ? edge_enums[(i+1)%4] : corner_enums[(i+1)%4];
 
-            int bits = b ? EDGE_BITS : CORNER_BITS;
+            // set all bits in clear mask
+            Int64 clear_mask = ((1 << CUBIE_BITS) - 1);
+ 
+            // set add mask to old cubie encoding
+            Int64 add_mask = getCubie((isEdge ? old_c.edges : old_c.corners), old_enum);
 
-            Int64 add_mask, clear_mask;
+            // update add mask to update cubie orientation
+            if (isEdge && (m & (U|D))) {
+                add_mask ^= 1 << (CUBIE_BITS - 1);
+            }
+            else if (!isEdge && 0) {
+                // TODO: figure out how and when to change corner orientation
+            }
 
-            add_mask = b ? old_c.edges : old_c.corners;
-            add_mask >>= bits * old_enum;
-            add_mask &= ((1 << bits) - 1);
-            add_mask <<= bits * new_enum;
+            // shift masks into encoding index of new cubie
+            clear_mask <<= CUBIE_BITS * new_enum;
+            add_mask <<= CUBIE_BITS * new_enum;
 
-            clear_mask = ((1 << bits) - 1);
-            clear_mask <<= bits * new_enum;
-
-            if (b) {
+            // update new cubie encoding by applying masks
+            if (isEdge) {
                 c.edges = (c.edges & ~clear_mask) | add_mask;
             }
             else {
@@ -131,7 +143,131 @@ int areEqual(Cube c1, Cube c2) {
     return (c1.edges == c2.edges && c1.corners == c2.corners);
 }
 
+static FaceEnum getColor(Cube c, FaceEnum fe, CubieEnum ce, int isEdge) {
+    Int8 cubie = getCubie(isEdge ? c.edges : c.corners, ce);
+
+    Int8 permutation = cubie & ((1 << (CUBIE_BITS - (isEdge ? 1 : 2))) - 1);
+    Int8 orientation = cubie >> CUBIE_BITS - (isEdge ? 1 : 2);
+
+    FaceEnum colors[3] = {};
+
+    if (isEdge) {
+        if (permutation == UF) {
+            colors[0] = U_FACE; colors[1] = F_FACE;
+        }
+        else if (permutation == UB) {
+            colors[0] = U_FACE; colors[1] = B_FACE;
+        }
+        else if (permutation == UR) {
+            colors[0] = U_FACE; colors[1] = R_FACE;
+        }
+        else if (permutation == UL) {
+            colors[0] = U_FACE; colors[1] = L_FACE;
+        }
+        else if (permutation == DF) {
+            colors[0] = D_FACE; colors[1] = F_FACE;
+        }
+        else if (permutation == DB) {
+            colors[0] = D_FACE; colors[1] = B_FACE;
+        }
+        else if (permutation == DR) {
+            colors[0] = D_FACE; colors[1] = R_FACE;
+        }
+        else if (permutation == DL) {
+            colors[0] = D_FACE; colors[1] = L_FACE;
+        }
+        else if (permutation == FR) {
+            colors[0] = F_FACE; colors[1] = R_FACE;
+        }
+        else if (permutation == FL) {
+            colors[0] = F_FACE; colors[1] = L_FACE;
+        }
+        else if (permutation == BR) {
+            colors[0] = B_FACE; colors[1] = R_FACE;
+        }
+        else if (permutation == BL) {
+            colors[0] = B_FACE; colors[1] = L_FACE;
+        }
+    }
+    else {
+        if (permutation == UFR) {
+            colors[0] = U_FACE; colors[1] = F_FACE; colors[2] = R_FACE;
+        }
+        else if (permutation == UFL) {
+            colors[0] = U_FACE; colors[1] = F_FACE; colors[2] = L_FACE;
+        }
+        else if (permutation == UBR) {
+            colors[0] = U_FACE; colors[1] = B_FACE; colors[2] = R_FACE;
+        }
+        else if (permutation == UBL) {
+            colors[0] = U_FACE; colors[1] = B_FACE; colors[2] = L_FACE;
+        }
+        else if (permutation == DFR) {
+            colors[0] = D_FACE; colors[1] = F_FACE; colors[2] = R_FACE;
+        }
+        else if (permutation == DFL) {
+            colors[0] = D_FACE; colors[1] = F_FACE; colors[2] = L_FACE;
+        }
+        else if (permutation == DBR) {
+            colors[0] = D_FACE; colors[1] = B_FACE; colors[2] = R_FACE;
+        }
+        else if (permutation == DBL) {
+            colors[0] = D_FACE; colors[1] = B_FACE; colors[2] = L_FACE;
+        }
+    }
+
+    return colors[orientation];
+}
+
+static void printColor(FaceEnum color) {
+    if (color == U_FACE) {
+        printf("\033[0;37m"); // white
+    }
+    else if (color == D_FACE) {
+        printf("\033[0;33m"); // yellow
+    }
+    else if (color == F_FACE) {
+        printf("\033[0;32m"); // green
+    }
+    else if (color == B_FACE) {
+        printf("\033[0;34m"); // blue
+    }
+    else if (color == R_FACE) {
+        printf("\033[0;31m"); // red
+    }
+    else if (color == L_FACE) {
+        printf("\033[0;35m"); // purple
+    }
+    printf("▣\033[0m");
+}
+
 void printCube(Cube c) {
+    FaceEnum faces[6] = { U_FACE, L_FACE, F_FACE, R_FACE, B_FACE, D_FACE };
+    CubieEnum cubies[3][6][3] = {
+        { { UBL, UB, UBR }, { UBL, UL, UFL }, { UFL, UF, UFR }, { UFR, UR, UBR }, { UBR, UB, UBL }, { DFL, DF, DFR } },
+        { { UL, -1, UR }, { BL, -1, FL }, { FL, -1, FR }, { FR, -1, BR }, { BR, -1, BL }, { DL, -1, DR } },
+        { { UFL, UF, UFR }, { DBL, DL, DFL }, { DFL, DF, DFR }, { DFR, DR, DBR }, { DBR, DB, DBL }, { DBL, DB, DBR } } 
+    };
+
+    printf("┌──┤U├──┬──┤L├──┬──┤F├──┬──┤R├──┬──┤B├──┬──┤D├──┐\n");
+
+    int i, j, k;
+    for (i=0 ; i<3 ; i++) {
+        for (j=0 ; j<6 ; j++) {
+            printf("│ ");
+            for (k=0 ; k<3 ; k++) {
+                int isEdge = ((i+k) % 2 == 1);
+                FaceEnum color = (i == 1 && k == 1) ? faces[j] : getColor(c, faces[j], cubies[i][j][k], isEdge);
+                printColor(color);
+                printf(" ", color);
+            }
+        }
+        printf("│\n");
+    }
+
+    printf("└───────┴───────┴───────┴───────┴───────┴───────┘\n");
+
+    // FOR DEBUGGING PURPOSES ONLY:
     printf("%llu %llu\n", c.edges, c.corners);
 }
 
