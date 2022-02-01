73dafb0a991c2593ff245f5bd6ba9b0ab69a4cb2
Tushar Khan <tkbot744@gmail.com>
2022-02-01 06:59:56 -0500

    motor code

--- src/robot.c
+++ src/robot.c
@@ -1,9 +1,36 @@
 #include "robot.h"
 
-void rotateCW(Motor m) {
+#define RPM 60          // desired speed of revolution; independent of motor
+#define MOTOR_STEPS 200 // number of steps per revolution; dependent on motor
+
+static MOTOR_DELAY = (1000*60)/(2*RPM*MOTOR_STEPS);
+
+static void rotateMotor(Motor m, Int8 isCW, Int8 isH) {
+    // set motor direction
+    digitalWrite(m.dir_pin, isCW);
     
+    // send step pulses to motor
+    int i;
+    for (i = 0 ; i < MOTOR_STEPS/(isH ? 2 : 4) ; i++) {
+        digitalWrite(m.step_pin, 1);
+        delay(MOTOR_DELAY);
+        digitalWrite(m.step_pin, 0);
+        delay(MOTOR_DELAY);
+    }
+}
+
+void rotateCW(Motor m) {
+    rotateMotor(m, 1, 0);
 }
 
 void rotateCCW(Motor m) {
+    rotateMotor(m, 0, 0);
+}
 
+void rotateCW2(Motor m) {
+    rotateMotor(m, 1, 1);
+}
+
+void rotateCCW2(Motor m) {
+    rotateMotor(m, 0, 1);
 }
