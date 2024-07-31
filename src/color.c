fd20ecdd95e860bc2dc9f2e34b2e8b80c2b43974
tk <tkbot744@gmail.com>
2024-07-31 05:49:37 -0400

    implemented colored-cube to cube conversion

--- src/color.c
+++ src/color.c
@@ -1,7 +1,7 @@
 #include "cube.h"
 #include <stdio.h>
 
-typedef MoveMask Color; // to use same enums
+typedef MoveMask Color; // to reuse cubie enums
 
 typedef struct {
     Color C, U, D, R, L, UR, UL, DR, DL;
@@ -11,127 +11,16 @@ typedef struct {
     ColorFace U, D, F, B, R, L;
 } ColorCube;
 
-static Int8 colorToCubie(ColorCube cc, CubieEnum ce, int isEdge) {
-    Int8 permutation, orientation;
-    
-    Color colors[3] = {};
-
-    // read colors
-    if (isEdge) {
-        if (ce == UF) {
-            colors[0] = cc.U.D; colors[1] = cc.F.U;
-        }
-        else if (ce == UB) {
-            colors[0] = cc.U.U; colors[1] = cc.B.U;
-        }
-        else if (ce == UR) {
-            colors[0] = cc.U.R; colors[1] = cc.R.U;
-        }
-        else if (ce == UL) {
-            colors[0] = cc.U.L; colors[1] = cc.L.U;
-        }
-        else if (ce == DF) {
-            colors[0] = cc.D.U; colors[1] = cc.F.D;
-        }
-        else if (ce == DB) {
-            colors[0] = cc.D.D; colors[1] = cc.B.D;
-        }
-        else if (ce == DR) {
-            colors[0] = cc.D.R; colors[1] = cc.R.D;
-        }
-        else if (ce == DL) {
-            colors[0] = cc.D.L; colors[1] = cc.L.D;
-        }
-        else if (ce == FR) {
-            colors[0] = cc.F.R; colors[1] = cc.R.L;
-        }
-        else if (ce == FL) {
-            colors[0] = cc.F.L; colors[1] = cc.L.R;
-        }
-        else if (ce == BR) {
-            colors[0] = cc.B.L; colors[1] = cc.R.R;
-        }
-        else if (ce == BL) {
-            colors[0] = cc.B.R; colors[1] = cc.L.L;
-        }
+static Int8 cornerTetrad(CubieEnum ce) {
+    if (ce == UFR || ce == DFL || ce == UBL || ce == DBR) {
+        return 1;
     }
     else {
-        // TODO
+        return 0;
     }
-
-    // set permutation
-    if (isEdge) {
-        if ((colors[0]|colors[1]) == (U|F)) {
-            permutation = UF;
-        }
-        else if ((colors[0]|colors[1]) == (U|B)) {
-            permutation = UB;
-        }
-        else if ((colors[0]|colors[1]) == (U|R)) {
-            permutation = UR;
-        }
-        else if ((colors[0]|colors[1]) == (U|L)) {
-            permutation = UL;
-        }
-        else if ((colors[0]|colors[1]) == (D|F)) {
-            permutation = DF;
-        }
-        else if ((colors[0]|colors[1]) == (D|B)) {
-            permutation = DB;
-        }
-        else if ((colors[0]|colors[1]) == (D|R)) {
-            permutation = DR;
-        }
-        else if ((colors[0]|colors[1]) == (D|L)) {
-            permutation = DL;
-        }
-        else if ((colors[0]|colors[1]) == (F|R)) {
-            permutation = FR;
-        }
-        else if ((colors[0]|colors[1]) == (F|L)) {
-            permutation = FL;
-        }
-        else if ((colors[0]|colors[1]) == (B|R)) {
-            permutation = BR;
-        }
-        else if ((colors[0]|colors[1]) == (B|L)) {
-            permutation = BL;
-        }
-    }
-    else {
-        // TODO
-    }
-
-    // set orientation
-    if (isEdge) {
-        orientation = (colors[0] & (L|R) || ((colors[0] & (F|B)) && (colors[1] & (U|D))));
-    }
-    else {
-        // TODO
-    }
-
-    // return cubie encoding
-    Int8 cubie;
-    setPermutation(&cubie, isEdge, permutation);
-    setOrientation(&cubie, isEdge, orientation);
-    return cubie;
 }
 
-static Cube128 convertColorCube(ColorCube cc) {
-    Cube128 c = { 0, 0 };
-
-    Int64 i;
-    for(i=0 ; i<NUM_EDGES ; i++) {
-        setCubie(&c.edges, i, colorToCubie(cc, i, 1));
-    }
-    for(i=0 ; i<NUM_CORNERS ; i++) {
-        setCubie(&c.corners, i, colorToCubie(cc, i, 0));
-    }
-
-    return c;
-}
-
-static Color cubieToColor(Cube128 c, CubieEnum ce, int isEdge, Color fm) {
+static Color encodeCubie(Cube128 c, CubieEnum ce, int isEdge, Color fm) {
     Int8 cubie = getCubie(isEdge ? c.edges : c.corners, ce);
     Int8 permutation = getPermutation(cubie, isEdge);
     Int8 orientation = getOrientation(cubie, isEdge);
@@ -212,66 +101,45 @@ static Color cubieToColor(Cube128 c, CubieEnum ce, int isEdge, Color fm) {
         }
 
         // set color index
-        int permutation_tetrad, ce_tetrad;
-
-        if (permutation == UFR || permutation == DFL || permutation == UBL || permutation == DBR) {
-            permutation_tetrad = 1;
-        }
-        else {
-            permutation_tetrad = 0;
-        }
-
-        if (ce == UFR || ce == DFL || ce == UBL || ce == DBR) {
-            ce_tetrad = 1;
-        }
-        else {
-            ce_tetrad = 0;
-        }
+        Int8 true_tetrad = cornerTetrad(permutation);
+        Int8 curr_tetrad = cornerTetrad(ce);
 
         if (fm & (F|B)) {
-            if (ce_tetrad == 1) {
-                if (orientation == 0) {
-                    idx = 0;
+            if (orientation == 0) {
+                idx = 0;
+            }
+            else if (orientation == 1) {
+                if (curr_tetrad == 0) {
+                    idx = (true_tetrad == curr_tetrad) ? 1 : 2;
                 }
-                else if (orientation == 1) {
-                    idx = (permutation_tetrad == ce_tetrad) ? 2 : 1;
-                }
-                else if (orientation == 2) {
-                    idx = (permutation_tetrad == ce_tetrad) ? 1 : 2;
+                else {
+                    idx = (true_tetrad == curr_tetrad) ? 2 : 1;
                 }
             }
-            else {
-                if (orientation == 0) {
-                    idx = 0;
+            else if (orientation == 2) {
+                if (curr_tetrad == 1) {
+                    idx = (true_tetrad == curr_tetrad) ? 1 : 2;
                 }
-                else if (orientation == 1) {
-                    idx = (permutation_tetrad == ce_tetrad) ? 1 : 2;
-                }
-                else if (orientation == 2) {
-                    idx = (permutation_tetrad == ce_tetrad) ? 2 : 1;
+                else {
+                    idx = (true_tetrad == curr_tetrad) ? 2 : 1;
                 }
             }
         }
         else if (fm & (R|L)) {
             if (orientation == 0) {
-                if (permutation_tetrad == ce_tetrad) {
-                    idx = 1;
-                }
-                else {
-                    idx = 2;
-                }
+                idx = (true_tetrad == curr_tetrad) ? 1 : 2;
             }
             else if (orientation == 1) {
-                if (ce_tetrad == 0) {
-                    idx = (permutation_tetrad == ce_tetrad) ? 2 : 1;
+                if (curr_tetrad == 0) {
+                    idx = (true_tetrad == curr_tetrad) ? 2 : 1;
                 }
                 else {
                     idx = 0;
                 }
             }
             else if (orientation == 2) {
-                if (ce_tetrad == 1) {
-                    idx = (permutation_tetrad == ce_tetrad) ? 2 : 1;
+                if (curr_tetrad == 1) {
+                    idx = (true_tetrad == curr_tetrad) ? 2 : 1;
                 }
                 else {
                     idx = 0;
@@ -280,27 +148,22 @@ static Color cubieToColor(Cube128 c, CubieEnum ce, int isEdge, Color fm) {
         }
         else if (fm & (U|D)) {
             if (orientation == 0) {
-                if (permutation_tetrad == ce_tetrad) {
-                    idx = 2;
-                }
-                else {
-                    idx = 1;
-                }
+                idx = (true_tetrad == curr_tetrad) ? 2 : 1;
             }
             else if (orientation == 1) {
-                if (ce_tetrad == 0) {
+                if (curr_tetrad == 0) {
                     idx = 0;
                 }
                 else {
-                    idx = (permutation_tetrad == ce_tetrad) ? 1 : 2;
+                    idx = (true_tetrad == curr_tetrad) ? 1 : 2;
                 }
             }
             else if (orientation == 2) {
-                if (ce_tetrad == 1) {
+                if (curr_tetrad == 1) {
                     idx = 0;
                 }
                 else {
-                    idx = (permutation_tetrad == ce_tetrad) ? 1 : 2;
+                    idx = (true_tetrad == curr_tetrad) ? 1 : 2;
                 }
             }
         }
@@ -309,7 +172,170 @@ static Color cubieToColor(Cube128 c, CubieEnum ce, int isEdge, Color fm) {
     return colors[idx];
 }
 
-static ColorCube convertCube(Cube128 c) {
+static Int8 decodeCubie(ColorCube cc, CubieEnum ce, int isEdge) {
+    Int8 permutation, orientation;
+    
+    Color colors[3] = {};
+
+    // get colors
+    if (isEdge) {
+        if (ce == FR) {
+            colors[0] = cc.F.R; colors[1] = cc.R.L;
+        }
+        else if (ce == FL) {
+            colors[0] = cc.F.L; colors[1] = cc.L.R;
+        }
+        else if (ce == UF) {
+            colors[0] = cc.F.U; colors[1] = cc.U.D;
+        }
+        else if (ce == DF) {
+            colors[0] = cc.F.D; colors[1] = cc.D.U;
+        }
+        else if (ce == BR) {
+            colors[0] = cc.B.L; colors[1] = cc.R.R;
+        }
+        else if (ce == BL) {
+            colors[0] = cc.B.R; colors[1] = cc.L.L;
+        }
+        else if (ce == UB) {
+            colors[0] = cc.B.U; colors[1] = cc.U.U;
+        }
+        else if (ce == DB) {
+            colors[0] = cc.B.D; colors[1] = cc.D.D;
+        }
+        else if (ce == UR) {
+            colors[0] = cc.U.R; colors[1] = cc.R.U;
+        }
+        else if (ce == UL) {
+            colors[0] = cc.U.L; colors[1] = cc.L.U;
+        }
+        else if (ce == DR) {
+            colors[0] = cc.D.R; colors[1] = cc.R.D;
+        }
+        else if (ce == DL) {
+            colors[0] = cc.D.L; colors[1] = cc.L.D;
+        }
+    }
+    else {
+        if (ce == UFR) {
+            colors[0] = cc.F.UR; colors[1] = cc.R.UL; colors[2] = cc.U.DR;
+        }
+        else if (ce == UFL) {
+            colors[0] = cc.F.UL; colors[1] = cc.L.UR; colors[2] = cc.U.DL;
+        }
+        else if (ce == UBR) {
+            colors[0] = cc.B.UL; colors[1] = cc.R.UR; colors[2] = cc.U.UR;
+        }
+        else if (ce == UBL) {
+            colors[0] = cc.B.UR; colors[1] = cc.L.UL; colors[2] = cc.U.UL;
+        }
+        else if (ce == DFR) {
+            colors[0] = cc.F.DR; colors[1] = cc.R.DL; colors[2] = cc.D.UR;
+        }
+        else if (ce == DFL) {
+            colors[0] = cc.F.DL; colors[1] = cc.L.DR; colors[2] = cc.D.UL;
+        }
+        else if (ce == DBR) {
+            colors[0] = cc.B.DL; colors[1] = cc.R.DR; colors[2] = cc.D.DR;
+        }
+        else if (ce == DBL) {
+            colors[0] = cc.B.DR; colors[1] = cc.L.DL; colors[2] = cc.D.DL;
+        }
+    }
+
+    // set permutation
+    if (isEdge) {
+        if ((colors[0]|colors[1]) == (U|F)) {
+            permutation = UF;
+        }
+        else if ((colors[0]|colors[1]) == (U|B)) {
+            permutation = UB;
+        }
+        else if ((colors[0]|colors[1]) == (U|R)) {
+            permutation = UR;
+        }
+        else if ((colors[0]|colors[1]) == (U|L)) {
+            permutation = UL;
+        }
+        else if ((colors[0]|colors[1]) == (D|F)) {
+            permutation = DF;
+        }
+        else if ((colors[0]|colors[1]) == (D|B)) {
+            permutation = DB;
+        }
+        else if ((colors[0]|colors[1]) == (D|R)) {
+            permutation = DR;
+        }
+        else if ((colors[0]|colors[1]) == (D|L)) {
+            permutation = DL;
+        }
+        else if ((colors[0]|colors[1]) == (F|R)) {
+            permutation = FR;
+        }
+        else if ((colors[0]|colors[1]) == (F|L)) {
+            permutation = FL;
+        }
+        else if ((colors[0]|colors[1]) == (B|R)) {
+            permutation = BR;
+        }
+        else if ((colors[0]|colors[1]) == (B|L)) {
+            permutation = BL;
+        }
+    }
+    else {
+        if ((colors[0]|colors[1]|colors[2]) == (U|F|R)) {
+            permutation = UFR;
+        }
+        else if ((colors[0]|colors[1]|colors[2]) == (U|F|L)) {
+            permutation = UFL;
+        }
+        else if ((colors[0]|colors[1]|colors[2]) == (U|B|R)) {
+            permutation = UBR;
+        }
+        else if ((colors[0]|colors[1]|colors[2]) == (U|B|L)) {
+            permutation = UBL;
+        }
+        else if ((colors[0]|colors[1]|colors[2]) == (D|F|R)) {
+            permutation = DFR;
+        }
+        else if ((colors[0]|colors[1]|colors[2]) == (D|F|L)) {
+            permutation = DFL;
+        }
+        else if ((colors[0]|colors[1]|colors[2]) == (D|B|R)) {
+            permutation = DBR;
+        }
+        else if ((colors[0]|colors[1]|colors[2]) == (D|B|L)) {
+            permutation = DBL;
+        }
+    }
+
+    // set orientation
+    if (isEdge) {
+        orientation = (colors[0] & (L|R) || ((colors[0] & (U|D)) && (colors[1] & (F|B))));
+    }
+    else {
+        Int8 true_tetrad = cornerTetrad(permutation);
+        Int8 curr_tetrad = cornerTetrad(ce);
+
+        if (colors[0] & (F|B)) {
+            orientation = 0;
+        }
+        else if (colors[0] & (R|L)) {
+            orientation = (true_tetrad == 1) ? 2 : 1;
+        }
+        else {
+            orientation = (true_tetrad == 1) ? 1 : 2;
+        }
+    }
+
+    // return cubie encoding
+    Int8 cubie;
+    setPermutation(&cubie, isEdge, permutation);
+    setOrientation(&cubie, isEdge, orientation);
+    return cubie;
+}
+
+static ColorCube encodeCube(Cube128 c) {
     ColorCube cc;
     
     // set center cubies
@@ -321,58 +347,72 @@ static ColorCube convertCube(Cube128 c) {
     cc.L.C = L;
     
     // set edge cubies
-    cc.U.D = cubieToColor(c, UF, 1, U);
-    cc.F.U = cubieToColor(c, UF, 1, F);
-    cc.U.U = cubieToColor(c, UB, 1, U);
-    cc.B.U = cubieToColor(c, UB, 1, B);
-    cc.U.R = cubieToColor(c, UR, 1, U);
-    cc.R.U = cubieToColor(c, UR, 1, R);
-    cc.U.L = cubieToColor(c, UL, 1, U);
-    cc.L.U = cubieToColor(c, UL, 1, L);
-    cc.D.U = cubieToColor(c, DF, 1, D);
-    cc.F.D = cubieToColor(c, DF, 1, F);
-    cc.D.D = cubieToColor(c, DB, 1, D);
-    cc.B.D = cubieToColor(c, DB, 1, B);
-    cc.D.R = cubieToColor(c, DR, 1, D);
-    cc.R.D = cubieToColor(c, DR, 1, R);
-    cc.D.L = cubieToColor(c, DL, 1, D);
-    cc.L.D = cubieToColor(c, DL, 1, L);
-    cc.F.R = cubieToColor(c, FR, 1, F);
-    cc.R.L = cubieToColor(c, FR, 1, R);
-    cc.F.L = cubieToColor(c, FL, 1, F);
-    cc.L.R = cubieToColor(c, FL, 1, L);
-    cc.B.L = cubieToColor(c, BR, 1, B);
-    cc.R.R = cubieToColor(c, BR, 1, R);
-    cc.B.R = cubieToColor(c, BL, 1, B);
-    cc.L.L = cubieToColor(c, BL, 1, L);
+    cc.U.D = encodeCubie(c, UF, 1, U);
+    cc.F.U = encodeCubie(c, UF, 1, F);
+    cc.U.U = encodeCubie(c, UB, 1, U);
+    cc.B.U = encodeCubie(c, UB, 1, B);
+    cc.U.R = encodeCubie(c, UR, 1, U);
+    cc.R.U = encodeCubie(c, UR, 1, R);
+    cc.U.L = encodeCubie(c, UL, 1, U);
+    cc.L.U = encodeCubie(c, UL, 1, L);
+    cc.D.U = encodeCubie(c, DF, 1, D);
+    cc.F.D = encodeCubie(c, DF, 1, F);
+    cc.D.D = encodeCubie(c, DB, 1, D);
+    cc.B.D = encodeCubie(c, DB, 1, B);
+    cc.D.R = encodeCubie(c, DR, 1, D);
+    cc.R.D = encodeCubie(c, DR, 1, R);
+    cc.D.L = encodeCubie(c, DL, 1, D);
+    cc.L.D = encodeCubie(c, DL, 1, L);
+    cc.F.R = encodeCubie(c, FR, 1, F);
+    cc.R.L = encodeCubie(c, FR, 1, R);
+    cc.F.L = encodeCubie(c, FL, 1, F);
+    cc.L.R = encodeCubie(c, FL, 1, L);
+    cc.B.L = encodeCubie(c, BR, 1, B);
+    cc.R.R = encodeCubie(c, BR, 1, R);
+    cc.B.R = encodeCubie(c, BL, 1, B);
+    cc.L.L = encodeCubie(c, BL, 1, L);
             
     // set corner cubies
-    cc.U.DR = cubieToColor(c, UFR, 0, U);
-    cc.F.UR = cubieToColor(c, UFR, 0, F);
-    cc.R.UL = cubieToColor(c, UFR, 0, R);
-    cc.U.DL = cubieToColor(c, UFL, 0, U);
-    cc.F.UL = cubieToColor(c, UFL, 0, F);
-    cc.L.UR = cubieToColor(c, UFL, 0, L);
-    cc.U.UR = cubieToColor(c, UBR, 0, U);
-    cc.B.UL = cubieToColor(c, UBR, 0, B);
-    cc.R.UR = cubieToColor(c, UBR, 0, R);
-    cc.U.UL = cubieToColor(c, UBL, 0, U);
-    cc.B.UR = cubieToColor(c, UBL, 0, B);
-    cc.L.UL = cubieToColor(c, UBL, 0, L);
-    cc.D.UR = cubieToColor(c, DFR, 0, D);
-    cc.F.DR = cubieToColor(c, DFR, 0, F);
-    cc.R.DL = cubieToColor(c, DFR, 0, R);
-    cc.D.UL = cubieToColor(c, DFL, 0, D);
-    cc.F.DL = cubieToColor(c, DFL, 0, F);
-    cc.L.DR = cubieToColor(c, DFL, 0, L);
-    cc.D.DR = cubieToColor(c, DBR, 0, D);
-    cc.B.DL = cubieToColor(c, DBR, 0, B);
-    cc.R.DR = cubieToColor(c, DBR, 0, R);
-    cc.D.DL = cubieToColor(c, DBL, 0, D);
-    cc.B.DR = cubieToColor(c, DBL, 0, B);
-    cc.L.DL = cubieToColor(c, DBL, 0, L);
+    cc.U.DR = encodeCubie(c, UFR, 0, U);
+    cc.F.UR = encodeCubie(c, UFR, 0, F);
+    cc.R.UL = encodeCubie(c, UFR, 0, R);
+    cc.U.DL = encodeCubie(c, UFL, 0, U);
+    cc.F.UL = encodeCubie(c, UFL, 0, F);
+    cc.L.UR = encodeCubie(c, UFL, 0, L);
+    cc.U.UR = encodeCubie(c, UBR, 0, U);
+    cc.B.UL = encodeCubie(c, UBR, 0, B);
+    cc.R.UR = encodeCubie(c, UBR, 0, R);
+    cc.U.UL = encodeCubie(c, UBL, 0, U);
+    cc.B.UR = encodeCubie(c, UBL, 0, B);
+    cc.L.UL = encodeCubie(c, UBL, 0, L);
+    cc.D.UR = encodeCubie(c, DFR, 0, D);
+    cc.F.DR = encodeCubie(c, DFR, 0, F);
+    cc.R.DL = encodeCubie(c, DFR, 0, R);
+    cc.D.UL = encodeCubie(c, DFL, 0, D);
+    cc.F.DL = encodeCubie(c, DFL, 0, F);
+    cc.L.DR = encodeCubie(c, DFL, 0, L);
+    cc.D.DR = encodeCubie(c, DBR, 0, D);
+    cc.B.DL = encodeCubie(c, DBR, 0, B);
+    cc.R.DR = encodeCubie(c, DBR, 0, R);
+    cc.D.DL = encodeCubie(c, DBL, 0, D);
+    cc.B.DR = encodeCubie(c, DBL, 0, B);
+    cc.L.DL = encodeCubie(c, DBL, 0, L);
     
     return cc;
+}
+
+static Cube128 decodeCube(ColorCube cc) {
+    Cube128 c = { 0, 0 };
+
+    Int64 i;
+    for(i=0 ; i<NUM_EDGES ; i++) {
+        setCubie(&c.edges, i, decodeCubie(cc, i, 1));
+    }
+    for(i=0 ; i<NUM_CORNERS ; i++) {
+        setCubie(&c.corners, i, decodeCubie(cc, i, 0));
+    }
+
+    return c;
 }
 
 static void printColor(Color color) {
@@ -444,18 +484,20 @@ static void printColorCube(ColorCube cc) {
 }
 
 void printCube(Cube128 c) {
-    ColorCube cc = convertCube(c);
+    ColorCube cc = encodeCube(c);
     printColorCube(cc);
 }
 
 int main() {
-    int n = 1524; // todo - shouldn't have to write 10 twice
+    int n = 200;
     Move ms_scramble[n];
     Cube128 c1 = scramble(solvedCube(), ms_scramble, n);
-    ColorCube cc = convertCube(c1);
-    Cube128 c2 = convertColorCube(cc);
+    ColorCube cc1 = encodeCube(c1);
+    Cube128 c2 = decodeCube(cc1);
     
     printCube(c1);
-    printColorCube(cc);
+    printColorCube(cc1);
     printCube(c2);
+
+    return !areEqual(c1, c2);
 }
