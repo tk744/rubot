9387ce51bf21b67b1d6260e2b92c60b7172cfcdb
tk <tkbot744@gmail.com>
2024-08-02 21:56:15 -0400

    minor update

--- src/main.c
+++ src/main.c
@@ -155,7 +155,7 @@ int main(int argc, char *argv[]) {
     Move ms[MAX_MOVES];
     int l = solve(c, ms);
     if (l == -1) {
-        printf("No solution found.\n");
+        fprintf(stderr, "No solution found.\n");
         return 1;
     }
 
